11a33a477adc36e630283bde30a124134f476fd9
radhoda <rod.hoda@gmail.com>
2024-12-15 17:02:39 -0800

    Implemented a class to parse leetcode questions and storer in class var.
    Up Next:
    Configure the database

---
+++ scrape_leetcode.py
@@ -0,0 +1,58 @@
+import requests
+
+class Scrape_Questions():
+    def __init__(self):
+        self.totalNumQuestions = 0
+        self.questionsList = []
+    def get_question_by_tag(self):
+        data = {
+            "query": """query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {
+                    problemsetQuestionList: questionList(
+                        categorySlug: $categorySlug
+                        limit: $limit
+                        skip: $skip
+                        filters: $filters
+                    ) {
+                        total: totalNum
+                        questions: data {
+                            acceptanceRate: acRate
+                            difficulty
+                            QID: questionFrontendId
+                            paidOnly: isPaidOnly
+                            title
+                            titleSlug
+                            topicTags {
+                                slug
+                            }
+                        }
+                    }
+                }
+            """,
+            "variables": {
+                "categorySlug": "",
+                "skip": 0,
+                "limit": 1000,
+                "filters": {}
+            },
+        }
+
+        r = requests.post("https://leetcode.com/graphql", json=data).json()
+        questionsList = r["data"]["problemsetQuestionList"]
+
+        self.parse_question_list(questionsList)
+
+
+    def parse_question_list(self, questionsData):
+        self.totalNumQuestions = questionsData["total"]
+        self.questionsList = questionsData["questions"]
+
+
+
+if __name__ == "__main__":
+    scraper = Scrape_Questions()
+
+    data = scraper.get_question_by_tag()
+
+    print("Done")
+
+
