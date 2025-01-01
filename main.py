659a09570491aa7032cd82e0543cebcf42a04028
radhoda <rod.hoda@gmail.com>
2024-12-31 18:28:45 -0800

    Setting up website using flask and creating an html folder to load the page

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from leetcode_data import scrape_leetcode, database
+from leetcode_data.database import DatabaseHandler, Question
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+db_handler = DatabaseHandler()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    query = request.form.get('query', '')
+
+    results = []
+    if query:
+        session = db_handler.session
+        results = session.query(Question).filter(Question.title.ilike(f"%{query}%")).all()
+
+    return render_template('results.html', query=query, results=results)
+
 
 if __name__ == "__main__":
-    scraper = scrape_leetcode.Scrape_Questions()
-    scraper.scrape_questions()
-    questions = scraper.get_questions()
-    databaseHandler = database.DatabaseHandler()
+    # scraper = scrape_leetcode.Scrape_Questions()
+    # scraper.scrape_questions()
+    # questions = scraper.get_questions()
 
-    databaseHandler.add_questions(questions)
-    question_list = databaseHandler.get_questions()
-
-    print("Anything")
+    # databaseHandler = database.DatabaseHandler()
+    # # databaseHandler.add_questions(questions)
+    # question_list = databaseHandler.get_questions()
+    #
+    # print("Anything")
+    app.run(debug=True)
